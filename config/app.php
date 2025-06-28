2b0faf271e111b9776d96ef3fbefc620ebba5800
smurak1201 <s.murakoshi1201@gmail.com>
2025-06-28 23:44:57 +0900

    ファイルのパーミッションを755に変更しました。

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
@@ -123,4 +122,19 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically registered
+    | when your application is started. You can add your own services
+    | to this array to have them loaded by the framework.
+    |
+    */
+
+    'providers' => [
+        // ...existing code...
+        App\Providers\RouteServiceProvider::class,
+    ],
 ];
