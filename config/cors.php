4ffdf647aa7b58f3d06dec53ac1c7ccdbd07bb74
smurak1201 <s.murakoshi1201@gmail.com>
2025-06-30 13:13:32 +0900

    cors.phpのallowed_originsにlocalhostの接続を追加し、インデントを修正しました

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,15 @@
 <?php
 
 return [
-  'paths' => ['api/*'],
-  'allowed_methods' => ['*'],
-  'allowed_origins' => ['http://172.18.119.226:5173'],
-  'allowed_origins_patterns' => [],
-  'allowed_headers' => ['*'],
-  'exposed_headers' => [],
-  'max_age' => 0,
-  'supports_credentials' => false,
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://172.18.119.226:5173',
+        'http://localhost:5173',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
