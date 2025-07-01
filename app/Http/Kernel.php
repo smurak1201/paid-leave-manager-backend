e8755ee1785ecc6a0dc37b4aad7e6e16624ffe24
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-01 13:46:23 +0900

    CORSミドルウェアをFruitcake\Cors\HandleCorsに変更しました

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // ...existing middleware...
-        \App\Http\Middleware\CorsMiddleware::class,
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
