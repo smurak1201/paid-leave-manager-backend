ec60daa3d9859bcc181400bf41ac1c18a4550936
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-01 14:01:03 +0900

    CORS設定に'sanctum/csrf-cookie'パスを追加し、全てのオリジンを許可しました

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://172.18.119.226:5173',
         'http://localhost:5173',
+        '*',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
