4e6c7dde387242374de7351c35ddcbd3486c0d40
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-01 15:04:51 +0900

    LeaveUsageControllerのルーティングを整理し、冗長なエンドポイントを削除しました

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,9 @@ Route::get('/leave-grant-master', [LeaveGrantMasterController::class, 'index']);
 // 有給取得履歴
 Route::get('/leave-usages', [LeaveUsageController::class, 'index']);
 Route::post('/leave-usages', [LeaveUsageController::class, 'store']);
+Route::post('/leave-usages/add', [LeaveUsageController::class, 'store']);
 Route::delete('/leave-usages', [LeaveUsageController::class, 'destroy']);
+Route::post('/leave-usages/delete', [LeaveUsageController::class, 'destroy']);
 Route::get('/leave-summary', [LeaveUsageController::class, 'showSummary']);
 
 // テスト用
