5cbbf872a35638ffd415a24f436eeed18ecca651
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-02 17:07:54 +0900

    LeaveGrantMasterSeederのコード整形を行い、可読性を向上させました。

--- database/seeders/LeaveGrantMasterSeeder.php
+++ database/seeders/LeaveGrantMasterSeeder.php
@@ -18,20 +18,20 @@ use App\Models\LeaveGrantMaster;
 
 class LeaveGrantMasterSeeder extends Seeder
 {
-  public function run()
-  {
-    $data = [
-      ['months' => 6, 'days' => 10],
-      ['months' => 18, 'days' => 11],
-      ['months' => 30, 'days' => 12],
-      ['months' => 42, 'days' => 14],
-      ['months' => 54, 'days' => 16],
-      ['months' => 66, 'days' => 18],
-      ['months' => 78, 'days' => 20],
-    ];
+    public function run()
+    {
+        $data = [
+            ['months' => 6, 'days' => 10],
+            ['months' => 18, 'days' => 11],
+            ['months' => 30, 'days' => 12],
+            ['months' => 42, 'days' => 14],
+            ['months' => 54, 'days' => 16],
+            ['months' => 66, 'days' => 18],
+            ['months' => 78, 'days' => 20],
+        ];
 
-    foreach ($data as $entry) {
-      LeaveGrantMaster::create($entry);
+        foreach ($data as $entry) {
+            LeaveGrantMaster::create($entry);
+        }
     }
-  }
 }
