2b68bb3e0451bc65c0a25c3d6383d05e9758d6be
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-02 16:43:54 +0900

    従業員APIおよび有給関連APIのルートを認証ミドルウェアでグループ化し、可読性を向上させました。

--- routes/api.php
+++ routes/api.php
@@ -21,18 +21,25 @@ use App\Http\Controllers\LeaveUsageController;
 
 // 従業員APIはEmployeesControllerに統一
 Route::middleware('auth:sanctum')->get('/employees', [EmployeesController::class, 'index']);
-Route::post('/employees', [EmployeesController::class, 'store']);
-Route::put('/employees/{id}', [EmployeesController::class, 'update']);
-Route::delete('/employees/{id}', [EmployeesController::class, 'destroy']);
 
-// 有給付与マスター
-Route::get('/leave-grant-master', [LeaveGrantMasterController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    // 従業員API
+    Route::post('/employees', [EmployeesController::class, 'store']);
+    Route::put('/employees/{id}', [EmployeesController::class, 'update']);
+    Route::delete('/employees/{id}', [EmployeesController::class, 'destroy']);
 
-// 有給取得履歴 RESTful API
-Route::get('/leave-usages', [LeaveUsageController::class, 'index']); // 一覧取得
-Route::post('/leave-usages', [LeaveUsageController::class, 'store']); // 追加
-Route::delete('/leave-usages/{id}', [LeaveUsageController::class, 'destroy']); // id指定で削除
-Route::get('/leave-summary', [LeaveUsageController::class, 'showSummary']);
+    // 有給付与マスター
+    Route::get('/leave-grant-master', [LeaveGrantMasterController::class, 'index']);
+    Route::post('/leave-grant-master', [LeaveGrantMasterController::class, 'store']);
+    Route::put('/leave-grant-master/{id}', [LeaveGrantMasterController::class, 'update']);
+    Route::delete('/leave-grant-master/{id}', [LeaveGrantMasterController::class, 'destroy']);
+
+    // 有給取得履歴 RESTful API
+    Route::get('/leave-usages', [LeaveUsageController::class, 'index']); // 一覧取得
+    Route::post('/leave-usages', [LeaveUsageController::class, 'store']); // 追加
+    Route::delete('/leave-usages/{id}', [LeaveUsageController::class, 'destroy']); // id指定で削除
+    Route::get('/leave-summary', [LeaveUsageController::class, 'showSummary']);
+});
 
 // テスト用
 Route::get('/test', function () {
