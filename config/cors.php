082b3a5b52094dd8c9282eb15e981ccb9994d36b
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-06 13:40:22 +0900

    CORS設定にAPIパスを追加しました。

--- config/cors.php
+++ config/cors.php
@@ -14,7 +14,7 @@
 // =====================================================
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'api', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://172.18.119.226:5173',
