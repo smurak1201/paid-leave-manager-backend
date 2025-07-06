bdaa5f20f141f0185358a8eedb6ab37a46d15753
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-06 12:17:43 +0900

    CORS設定ファイルのコメントを整理し、可読性を向上させました。

--- config/cors.php
+++ config/cors.php
@@ -14,16 +14,15 @@
 // =====================================================
 
 return [
-  'paths' => ['api/*', 'sanctum/csrf-cookie'],
-  'allowed_methods' => ['*'],
-  'allowed_origins' => [
-    'http://172.18.119.226:5173',
-    'http://localhost:5173',
-    '*',
-  ],
-  'allowed_origins_patterns' => [],
-  'allowed_headers' => ['*'],
-  'exposed_headers' => [],
-  'max_age' => 0,
-  'supports_credentials' => false,
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://172.18.119.226:5173',
+        'http://localhost:5173',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
 ];
