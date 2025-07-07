ac4e535ad3be6092111a15ac781851694245913d
smurak1201 <s.murakoshi1201@gmail.com>
2025-07-07 15:50:19 +0900

    AdminEmployeeSeederを追加し、管理者従業員のデータベースシードを実装

--- database/seeders/AdminEmployeeSeeder.php
+++ database/seeders/AdminEmployeeSeeder.php
@@ -6,15 +6,18 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class AdminUserSeeder extends Seeder
+class AdminEmployeeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('users')->insert([
+        DB::table('employees')->insert([
             'employee_id' => 0,
+            'last_name' => '管理',
+            'first_name' => '者',
+            'joined_at' => now(),
             'password' => Hash::make('p@ssw0rd'),
             'role' => 'admin',
             'created_at' => now(),
