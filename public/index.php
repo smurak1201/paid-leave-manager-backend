96064367d15c7685bb4ac7ebc92584302b738765
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-11 20:07:22 +0900

    index.phpのパスを修正し、メンテナンスモードのチェックを更新

--- public/index.php
+++ public/index.php
@@ -7,17 +7,17 @@ define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 // if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
-if (file_exists($maintenance = __DIR__ . '/paid-leave-manager/backend/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/backend/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
 // require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/paid-leave-manager/backend/vendor/autoload.php';
+require __DIR__ . '/backend/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 // $app = require_once __DIR__ . '/../bootstrap/app.php';
-$app = require_once __DIR__ . '/paid-leave-manager/backend/bootstrap/app.php';
+$app = require_once __DIR__ . '/backend/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
